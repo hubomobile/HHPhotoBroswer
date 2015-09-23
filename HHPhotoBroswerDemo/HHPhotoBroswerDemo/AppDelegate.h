33749a692bb7cb98be76f70df65056396f597ebb
hubo <hubomobile@126.com>
2015-09-23 11:33:02 +0800

    Commit. New a project.

---
+++ HHPhotoBroswerDemo/HHPhotoBroswerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHPhotoBroswerDemo
+//
+//  Created by hubo on 15/9/23.
+//  Copyright © 2015年 hubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
